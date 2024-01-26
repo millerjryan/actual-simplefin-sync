645986c644bf8183e6c47de1b515f70ca2df4afb
millerjryan <50216489+millerjryan@users.noreply.github.com>
2024-01-25 19:44:13 -0500

    Update simpleFIN.js

--- simpleFIN.js
+++ simpleFIN.js
@@ -40,7 +40,7 @@ async function getAccessKey (base64Token) {
 
 async function getTransactions (accessKey, startDate, endDate) {
   const now = new Date()
-  startDate = startDate || new Date(now.getFullYear(), now.getMonth(), -1, 1)
+  startDate = startDate || new Date(now.getFullYear(), now.getMonth(), -31, 1)
   endDate = endDate || new Date(now.getFullYear(), now.getMonth() + 1, 1)
   console.log(`${startDate.toISOString().split('T')[0]} - ${endDate.toISOString().split('T')[0]}`)
   return await getAccounts(accessKey, startDate, endDate)
